e739ab4170091a21ea51bf4962576cf89dc90237
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-09-29 22:28:23 -0400

    started array processing

--- app/js/app.js
+++ app/js/app.js
@@ -3,14 +3,16 @@ var app = angular.module("ourApp", []);
 app.controller("mainController", function ($scope, $http) {
 
 	$scope.input = 'Marc';
-
+	$scope.showResults = true;
 
 	$scope.submit = function () { 
 
 		$http.get("https://maps.googleapis.com/maps/api/geocode/json?address='" + $scope.address + "'").then(
 			function (data) {
-				$scope.data = data.data;
+				var array = data.data;
+				$scope.data = array[1];
 				console.log($scope.data);
+				$scope.showResults = false;
 			}, function (err) {
 				$scope.data = err;
 			}
